c63c9e3cc3c788b19529c5ca109db40fc8ba8ac5
SandovalJr <edubizzle7@gmail.com>
2022-03-04 11:52:40 -0600

    params

--- src/controllers/twitter.controller.js
+++ src/controllers/twitter.controller.js
@@ -1,7 +1,24 @@
+// params
+
 export const postTwitter1 = (req, res) => {
-  const inf = { name: "sandoval", mode: "public", description: "prueba" };
-  return res.status(200).json(inf);
+  const nombre = req.query.name;
+  const mode = req.query.mode;
+  const description = req.query.description;
+  // console.log("llego");
+  // console.log(nombre);
+  res.send({
+    name: nombre,
+    mode: mode,
+    description: description,
+  });
+
+  // return res.status(200).json({ name });
 };
+
+// export const postTwitter1 = (req, res) => {
+//   const inf = { name: "sandoval", mode: "public", description: "prueba" };
+//   return res.status(200).json(inf);
+// };
 
 export const postTwitter2 = (req, res) => {
   return res.status(200).json({
